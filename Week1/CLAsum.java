794102f0ac37c3322257256361e441ce4ee91b64
srisanthy <srisanthy86@gmail.com>
2026-07-12 13:09:06 +0530

    Create CLAsum.java

---
+++ Week1/CLAsum.java
@@ -0,0 +1,7 @@
+class ClSum {
+  public static void main(String[] args) {
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    System.out.println(&quot;Addition of &quot; + a + &quot; and &quot; + b + &quot; is &quot; + (a + b));
+  }
+}
