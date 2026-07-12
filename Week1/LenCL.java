aeed0ad4048a4fe3b5a40b3ba3aa75c6e0a9bf8c
srisanthy <srisanthy86@gmail.com>
2026-07-12 13:16:36 +0530

    Create LenCL.java

---
+++ Week1/LenCL.java
@@ -0,0 +1,6 @@
+public class ClLen {
+    public static void main(String[] args) {
+        int a = args.length;
+        System.out.println(&quot;Numbers of arguments in Command Line is &quot; + a);
+    }
+}
