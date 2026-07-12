a98e324843ae5433fc0f4547f5c24ef47d63a643
srisanthy <srisanthy86@gmail.com>
2026-07-12 13:11:45 +0530

    Create DefValue.java

---
+++ Week1/DefValue.java
@@ -0,0 +1,12 @@
+public class StaticDefVal {
+    static int a;
+    static float b;
+    static double c;
+    static long d;
+    static short e;
+    static byte f;
+    static boolean g;
+    public static void main(String[] args) {
+        System.out.println(a + &quot; &quot; + b + &quot; &quot; + c + &quot; &quot; + d + &quot; &quot; + e + &quot; &quot; + f + &quot; &quot; + g);
+    }
+}
