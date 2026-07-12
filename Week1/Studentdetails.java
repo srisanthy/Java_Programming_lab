13c51f1fab2814da81fca6ad211451aade4f5c76
srisanthy <srisanthy86@gmail.com>
2026-07-12 13:14:47 +0530

    Create Studentdetails.java

---
+++ Week1/Studentdetails.java
@@ -0,0 +1,6 @@
+class Studentdetails {
+  public static void main(String[] args) {
+    System.out.println(&quot;Roll no.: &quot; + args[0]);
+    System.out.println(&quot;Name: &quot; + args[1]);
+  }
+}
