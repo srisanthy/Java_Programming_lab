d1a74c32d5666a26b98d29badc28c40e051666ac
srisanthy <srisanthy86@gmail.com>
2026-07-15 19:55:37 +0530

    Update Student.java

--- Week2/Student.java
+++ Week2/Student.java
@@ -3,7 +3,7 @@ public class Student {
   String name, dept;
   public static void main(String[] args) {
     Student s = new Student();
-    s.rollno = 27;
+    s.rollno = 30;
     s.name = &quot;ABC&quot;;
     s.dept = &quot;CSE&quot;;
     System.out.println(&quot;Name is &quot; + s.name + &quot; bearing Roll Number &quot; + s.rollno + &quot; in Department &quot; +
