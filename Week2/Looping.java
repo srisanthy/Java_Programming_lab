4bf20b86ec8e69bbc899a3f0dca218f8c88d5a15
srisanthy <srisanthy86@gmail.com>
2026-07-16 22:37:39 +0530

    Create Looping.java

---
+++ Week2/Looping.java
@@ -0,0 +1,15 @@
+public class LoopStat {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0, j = 0;
+		for(int a = 0; a < 5; a++) {
+			System.out.println(a);
+		}
+		while(i < 3) {
+			System.out.println(i);
+		}
+		do {
+			System.out.println(j);
+		} while(j > 3);
+	}
+}
