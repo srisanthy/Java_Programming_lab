4271c3b700ff02225840ebfd6d6f7775fec9f5d6
srisanthy <srisanthy86@gmail.com>
2026-07-16 22:31:54 +0530

    Create Student.java

---
+++ Week2/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+	int rollno;
+	String name, dept;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student();
+		s.rollno = 30;
+		s.name = "SRI";
+		s.dept = "CSE";
+		System.out.println("Name is " + s.name + " bearing Roll Number " + s.rollno + " in Department " + s.dept);
+	}
+
+}
